a777702f019747dbbbad5aa91c275b8284c4fa2a
Ji Yim <gsooyim@gmail.com>
2018-04-19 18:46:09 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |name, data|
     if name == "Freddy Mercury"
       data.each do |attribute, info|
-        if info == :favorite_ice_cream_flavors
+        if attribute == :favorite_ice_cream_flavors
           info.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
       end
